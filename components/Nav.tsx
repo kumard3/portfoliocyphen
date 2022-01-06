10308029cfce40352ea759ecea6ba93524d2e265
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-06 05:42:07 +0530

    nav drawer added

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,24 +1,24 @@
-import { Fragment } from "react";
-
-import { Popover, Transition } from "@headlessui/react";
+import { useState } from "react";
+import { Drawer } from "@mantine/core";
 
 import Link from "next/link";
 
 const navlink = [
   {
-    name: "About",
+    name: "about",
     href: "#about",
   },
   {
-    name: "Contact",
+    name: "contact",
     href: "#contact",
   },
 ];
 
 export default function Nav() {
+  const [opened, setOpened] = useState(false);
   return (
     // <Popover className= {`sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-gray-900/10 bg-white supports-backdrop-blur:bg-white/95 ${show && 'bg-inherit	'}`}>
-    <Popover className="sticky top-0 z-50 bg-white backdrop-blur-sm border-black border-b-[1px] text-xl font-bold  flex-none transition-colors duration-500 lg:z-50 ">
+    <div className="sticky top-0 z-50 bg-white backdrop-blur-sm border-black border-b-[1px] text-xl font-bold  flex-none transition-colors duration-500 lg:z-50 ">
       <div className="w-full container mx-auto">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 ">
           <div className="flex justify-between items-center   py-6 sm:justify-between sm:space-x-10">
@@ -32,9 +32,12 @@ export default function Nav() {
               </Link>
             </div>
             <div className="-mr-2 -my-2 sm:hidden">
-              <Popover.Button className=" rounded-md p-2 inline-flex items-center justify-center text-black hover:text-gray-500 ">
+              <div
+                onClick={() => setOpened(true)}
+                className=" rounded-md p-2 inline-flex items-center justify-center text-black hover:text-gray-500 "
+              >
                 <button
-                  type="button"
+              
                   className="inline-flex items-center lg:hidden text-black  focus-visible:ring ring-indigo-300  active:text-gray-700 text-sm md:text-base font-semibold rounded-lg gap-2 px-2.5 py-2"
                 >
                   <svg
@@ -52,10 +55,10 @@ export default function Nav() {
                     />
                   </svg>
                 </button>
-              </Popover.Button>
+              </div>
             </div>
-            <Popover.Group
-              as="nav"
+            <nav
+           
               className="hidden sm:flex space-x-10 items-center"
             >
               {navlink.map((n) => {
@@ -65,64 +68,24 @@ export default function Nav() {
                   </Link>
                 );
               })}
-            </Popover.Group>
+            </nav>
           </div>
         </div>
 
-        <Transition
-          as={Fragment}
-          enter="duration-200 ease-out"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="duration-100 ease-in"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <Popover.Panel
-            focus
-            className="absolute top-0  inset-x-0 p-2 h-[100vh] transition transform origin-top-right md:hidden"
+
+          <Drawer
+            opened={opened}
+            onClose={() => setOpened(false)}
+            padding="xl"
+            position="right" size="lg"
           >
-            <div className="rounded-lg shadow-lg ring-1 h-full ring-black ring-opacity-5 bg-gray-200  text-black divide-y-2 divide-gray-50">
-              <div className="pt-5 pb-6 px-5">
-                <div className="flex items-center justify-between">
-                  <div className="text-white">
-                    <Link href="/">
-                      <img
-                        src="/cyphen-logo.png"
-                        alt="logo"
-                        className="h-[3.5rem]"
-                      />
-                      {/* <span className="text-4xl font-bold">logo</span> */}
-                    </Link>
-                  </div>
-                  <div className="-mr-2">
-                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-black hover:text-gray-500 hover:bg-gray-100 ">
-                      <span className="sr-only">Close menu</span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                        className="h-6 w-6"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        ></path>
-                      </svg>
-                    </Popover.Button>
-                  </div>
-                </div>
                 <div className="mt-6">
                   <nav className="grid gap-y-8">
                     {navlink.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
-                        className="-m-3 p-3 flex items-center rounded-md hover:bg-black"
+                        className="-m-3 p-3 flex items-center rounded-md hover:bg-black hover:text-white"
                       >
                         <h1 className="my-3 ml-3 text-3xl font-bold ">
                           {item.name}
@@ -130,13 +93,16 @@ export default function Nav() {
                       </a>
                     ))}
                   </nav>
-                </div>
-              </div>
+             
             </div>
-          </Popover.Panel>
-        </Transition>
+          </Drawer>
+          {/* <Popover.Panel
+            focus
+            className="absolute top-0  inset-x-0 p-2 h-[100vh] transition transform origin-top-right md:hidden"
+          ></Popover.Panel> */}
+  
       </div>
-    </Popover>
+    </div>
   );
 }
 
--- next.config.js
+++ next.config.js
@@ -2,3 +2,13 @@
 module.exports = {
   reactStrictMode: true,
 }
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+});
