0a24251a9ce91104405b5e748eec2450ec65d934
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-02 12:37:21 +0530

    new service section added

--- components/Service.tsx
+++ components/Service.tsx
@@ -1,87 +1,39 @@
 import React from "react";
+import { ServiceData } from "../data/data";
 
 export default function Service() {
   return (
     <div
       id="services"
-      className="flex  flex-col justify-center items-center w-full   text-2xl sm:text-3xl "
+      className="flex  flex-col justify-center items-center w-full "
     >
       <section className=" w-full mt-[10rem] ">
-        <h1 className="text-center font-mono gradientText text-5xl">
-          Our Services
-        </h1>
-
-        <div className="p-4 lg:p-8 container mx-auto">
-          <div
-            data-aos="zoom-in"
-            className="flex flex-col overflow-hidden md shadow-sm bg-slate-700/10 rounded-3xl my-4 lg:flex-row"
-          >
-            <div className="flex items-center justify-center flex-1 ">
-              <img src="/Userinterface_Flatline.png" alt="" className=" " />
-            </div>
-            <div className="flex flex-col justify-center flex-1 px-6 py-12 ">
-              {/* <span className="text-xs uppercase dark:text-coolGray-400">Join, it's free</span> */}
-              <h2 className="text-3xl font-bold">Mobile Development</h2>
-              <p className="my-6 dark:text-coolGray-400">
-                We specialize in building native mobile apps for Android and iOS
-                using React Native.
-              </p>
-              {/* <button type="button" className="self-start">Action</button> */}
-
-              <ul className="list-disc">
-                <li>React Native Development</li> <li>API Development</li>
-                <li>Backend Development</li>
-                <li>3rd party Integrations</li>
-              </ul>
-            </div>
-          </div>
-          <div
-            data-aos="zoom-in"
-            className="flex flex-col overflow-hidden md shadow-sm bg-slate-700/10 rounded-3xl my-4 lg:flex-row-reverse "
-          >
-            <div className="flex items-center justify-center flex-1 ">
-              <img src="/Designer _Flatline.png" alt="" className=" " />
-            </div>
-            <div className="flex flex-col justify-center flex-1 px-12 py-12 ">
-              {/* <span className="text-xs uppercase dark:text-coolGray-400">Join, it's free</span> */}
-              <h2 className="text-3xl font-bold">Web Development</h2>
-              <p className="my-6 dark:text-coolGray-400">
-                From simple websites to online stores to custom built
-                applications, we do it all.
-              </p>
-              <ul className="list-disc">
-                <li>Content Management Systems</li>
-                <li>Payment Processor Integrations</li>
-                <li>eCommerce Solutions</li>
-                <li>Search Engine Optimization</li>
-                <li>Web Design</li>
-              </ul>
-              {/* <button type="button" className="self-start">Action</button> */}
-            </div>
-          </div>
-
-          <div
-            data-aos="zoom-in"
-            className="flex flex-col overflow-hidden md shadow-sm bg-slate-700/10 rounded-3xl my-4 lg:flex-row "
-          >
-            <div className="flex items-center justify-center flex-1 ">
-              <img src="/Instagrampost _Flatline.png" alt="" className="" />
-            </div>
-            <div className="flex flex-col justify-center flex-1 px-6 py-12 ">
-              {/* <span className="text-xs uppercase dark:text-coolGray-400">Digital Markting</span> */}
-              <h2 className="text-3xl font-bold">Digital Markting</h2>
-              <p className="my-6 dark:text-coolGray-400">
-                We convert business into a brand through a recise Digital
-                marketing strategy.
-              </p>
-              {/* <button type="button" className="self-start">Action</button> */}
-              <ul className="list-disc">
-                <li>Content Management Systems</li>{" "}
-                <li>Content Management Systems</li>{" "}
-                <li>Content Management Systems</li>
-              </ul>
-            </div>
-          </div>
+        <div className="flex justify-center items-center sm:justify-between px-10 sm:flex-row  flex-col">
+          <h1 className="text-center font-mono gradientText  text-5xl">
+            Our Services
+          </h1>
+          <button className="gradientbutton px-10 w-[10rem] py-[.5em] rounded-xl text-xl font-mono">
+            {" "}
+            <a href="/#contact"> Service</a>{" "}
+          </button>
+        </div>
+        <div className="p-4 lg:p-8 container justify-center items-center flex flex-wrap mx-auto">
+          {ServiceData.map((n, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col overflow-hidden h-[24rem] max-w-[400px] shadow-sm bg-slate-700/10 rounded-3xl my-4 "
+              >
+                <div className="flex items-center justify-center flex-1 ">
+                  <img src={n.image} alt="" className=" " />
+                </div>
+                <div className=" flex flex-col justify-center  bg-black/10 backdrop-blur-xl relative rounded-b-xl -top-[8rem] flex-1  px-10 py-5">
+                  <h2 className="text-3xl font-mono font-bold">{n.title}</h2>
+                  <p className=" text-xl">{n.shortDescription}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </section>
     </div>
---
+++ data/data.ts
@@ -0,0 +1,18 @@
+export const ServiceData = [
+    {
+        title: 'Mobile Development',
+        shortDescription: ' We specialize in building native mobile apps for Android and iOS using React Native.',
+        image: '/Userinterface_Flatline.png'
+    },
+    {
+        title: 'Web Development',
+        shortDescription: ' From simple websites to online stores to custom built applications, we do it all.',
+        image: '/Designer _Flatline.png'
+    },
+    {
+        title: 'Digital Markting',
+        shortDescription: ' We convert business into a brand through a recise Digital marketing strategy.',
+        image: '/Instagrampost _Flatline.png'
+    },
+
+]
